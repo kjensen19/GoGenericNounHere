809407127435dd5edc8817c60ba4462336a4d704
Kyle Jensen <kjensen19@gmail.com>
2023-01-08 00:17:00 -0600

    added generic function to do both

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ func main() {
 	fmt.Printf("Non-Generic Sums: %v and %v\n",
 		SumInts(ints),
 		SumFloats(floats))
+	fmt.Printf("generic Sums: %v and %v\n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats))
 }
 
 // adds together the values of m
@@ -32,6 +35,17 @@ func SumInts(m map[string]int64) int64 {
 // SumFloats adds togehter the values of m
 func SumFloats(m map[string]float64) float64 {
 	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumIntsOrFloats sums the values of a map m. Supports both int64 and float64 as types for m
+//
+//	⬇️Means any type that can be compared with == and !=
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
 	for _, v := range m {
 		s += v
 	}
